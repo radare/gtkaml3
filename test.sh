71246221663d46cc58a728158348654abffb108e
trufae <trufae@6bdb3c60-4643-0410-86bb-99cbaf4d9aa6>
2012-05-29 08:58:18 +0000

    Add bugs/

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 rm test.vala
 touch test.vala
-src/gtkamlc  test.gtkaml --save-temps --pkg gtk+-2.0 --dump-tree test.vala
+src/gtkamlc  test.gtkaml --save-temps --pkg gtk+-2.0 --dump-tree test.tree
 #gcc test.c test.h -I `pkg-config --cflags gtk+-2.0` -o test -L `pkg-config --libs gtk+-2.0` &2>1
 #cat test.vala
